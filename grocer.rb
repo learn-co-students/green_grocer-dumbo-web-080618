f587cb4fc90a96fd0327e7a6497aad3963b8725f
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-08-01 19:14:39 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,57 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |hash|
+    hash.each  do |item , stats|
+      if new_hash[item]
+        new_hash[item][:count] += 1
+      else
+        new_hash[item] = stats
+        new_hash[item][:count] = 1
+      end
+    end
+  end
+  new_hash
 end
 
+[{:item => "Avocado", :num=>2, :cost => 5}]
+
 def apply_coupons(cart, coupons)
-  # code here
+  return cart  if coupons == []
+  new_cart  = cart
+  coupons.each do |hash|
+    
+    num = hash[:num]
+    name = hash[:item]
+    
+    cart.to_a.each do |item , stats|
+      if name == item && stats[:count] >= num
+        new_cart[item][:count] -= num
+        if new_cart["#{item} W/COUPON"]
+          new_cart["#{item} W/COUPON"][:count] += 1
+        else
+          new_cart["#{item} W/COUPON"] = {
+          :price => hash[:cost],
+          :clearance => cart[item][:clearance],
+          :count => 1
+          }
+        end
+      end
+    end
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
-  # code here
+  new_cart = cart 
+ 
+  cart.each do |item , stats|
+    if stats[:clearance]
+      new_cart[item][:price] = (stats[:price] * 0.8).round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
 end
